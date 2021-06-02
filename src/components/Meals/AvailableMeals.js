a47a3a9b7eba73d858225ecb307d753310955fd8
JeffreyChen <jeffreychen@JeffreyChens-MacBook-Pro.local>
2021-06-02 10:20:29 -0500

    feat: add loading and error message

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,9 +33,25 @@ import appSetting from "../../appSetting";
 
 export default function AvailableMeals() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
+  // const pause = (ms) => {
+  //   return new Promise((res, rej) => {
+  //     setTimeout(() => {
+  //       res();
+  //     }, ms);
+  //   });
+  // };
+
   useEffect(() => {
+    setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch(appSetting.firebaseUrl);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const responseData = await response.json();
 
@@ -53,12 +69,38 @@ export default function AvailableMeals() {
       console.log(loadedMeals);
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    // *** IMPORTANT ***
+    // fetchMeals return promise, that means to catch error
+    // we will need to await for it
+    // however, it is allowed for useEffect to take in async function
+    // so, we will use the traditional way to handle the error
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
     // we only run the fetchMeals function when page load first time
     // so, we do not need to pass any dependencies
   }, []);
+
+  // conditional rendering
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
